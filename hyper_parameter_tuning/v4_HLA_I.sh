b66bc71db9afa123f5469594e8685cc008e17743
liusi2019 <liusi2019@gmail.com>
2022-01-09 00:23:10 -0800

    clean up command lines

--- hyper_parameter_tuning/v4_HLA_I.sh
+++ hyper_parameter_tuning/v4_HLA_I.sh
@@ -11,12 +11,7 @@ for enc_method in one_hot blosum62 atchley pca; do
             for n_units in [16] [32] [64]; do
               for dropout_flag in True; do
                 for p_dropout in 0.2 0.5; do
-                  . ../../anaconda3/etc/profile.d/conda.sh
-                  conda activate sliu3_gpu
-                  export OMP_NUM_THREADS=1
-                  sbatch --gres=gpu -t 01-10:00:00 -o v4.log \
-                          -p campus-new --job-name=v4 \
-                          v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
+                  v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
                 done
               done
             done
@@ -36,12 +31,7 @@ for enc_method in one_hot blosum62 atchley pca; do
             for n_units in [32,16] [64,16]; do
               for dropout_flag in True; do
                 for p_dropout in 0.2 0.5; do
-                  . ../../anaconda3/etc/profile.d/conda.sh
-                  conda activate sliu3_gpu
-                  export OMP_NUM_THREADS=1
-                  sbatch --gres=gpu -t 01-10:00:00 -o v4.log \
-                          -p campus-new --job-name=v4 \
-                          v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
+                  v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
                 done
               done
             done
@@ -61,12 +51,7 @@ for enc_method in one_hot blosum62 atchley pca; do
             for n_units in [16] [32] [64]; do
               for dropout_flag in False; do
                 for p_dropout in 0.2; do
-                  . ../../anaconda3/etc/profile.d/conda.sh
-                  conda activate sliu3_gpu
-                  export OMP_NUM_THREADS=1
-                  sbatch --gres=gpu -t 01-10:00:00 -o v4.log \
-                          -p campus-new --job-name=v4 \
-                          v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
+                  v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
                 done
               done
             done
@@ -86,12 +71,7 @@ for enc_method in one_hot blosum62 atchley pca; do
             for n_units in [32,16] [64,16]; do
               for dropout_flag in False; do
                 for p_dropout in 0.2; do
-                  . ../../anaconda3/etc/profile.d/conda.sh
-                  conda activate sliu3_gpu
-                  export OMP_NUM_THREADS=1
-                  sbatch --gres=gpu -t 01-10:00:00 -o v4.log \
-                          -p campus-new --job-name=v4 \
-                          v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
+                  v4_template_HLA_I_gpu.py $enc_method $n_fold $lr $V_cdrs $CNN_flag $n_dense $n_units $dropout_flag $p_dropout 1216 2207
                 done
               done
             done
